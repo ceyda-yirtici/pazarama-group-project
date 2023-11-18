7794105e880d15ded1a230b4b5134ebc44e38081
Enes CAN <enesscann98@gmail.com>
2023-11-18 15:01:11 +0300

    searchview listener added

--- app/src/main/java/com/example/moviesapp/presentation/view/HomeScreenFragment.kt
+++ app/src/main/java/com/example/moviesapp/presentation/view/HomeScreenFragment.kt
@@ -1,11 +1,11 @@
 package com.example.moviesapp.presentation.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviesapp.databinding.FragmentHomeScreenBinding
@@ -33,6 +33,23 @@ class HomeScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initView()
         initListener()
+
+        binding.searchView.setOnQueryTextListener (object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    viewModel.loadData(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {
+                    viewModel.loadData(newText)
+                }
+                return true
+            }
+        })
     }
 
 
