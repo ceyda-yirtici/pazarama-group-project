c8b9a4f0fa015f83774cc34574a0c8a161cf693b
Cumhur Günay <cugo2435@gmail.com>
2023-11-18 14:09:18 +0300

    MovieAdapter update

--- app/src/main/java/com/example/moviesapp/presentation/view/MovieAdapter.kt
+++ app/src/main/java/com/example/moviesapp/presentation/view/MovieAdapter.kt
@@ -3,11 +3,13 @@ package com.example.moviesapp.presentation.view
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.moviesapp.databinding.MovieRowBinding
+import com.example.moviesapp.domain.model.Search
 
 class MovieAdapter (var mContext: Context,
-                    var movieList: List<String>)
+                    var movieList: List<Search>)
     :RecyclerView.Adapter<MovieAdapter.MovieHolder>(){
 
     inner class MovieHolder(var binding: MovieRowBinding) : RecyclerView.ViewHolder(binding.root)
@@ -24,5 +26,13 @@ class MovieAdapter (var mContext: Context,
     override fun onBindViewHolder(holder: MovieHolder, position: Int) {
         val movie = movieList[position]
         val movieRow = holder.binding
+        movieRow.textViewTitle.text = movie.Title
+        movieRow.textViewYear.text = movie.Year
+
+        movieRow.cardViewMovie.setOnClickListener {
+            val goTo = HomeScreenFragmentDirections.homeToDetail(movieId = movie.imdbID)
+            Navigation.findNavController(it).navigate(goTo)
+        }
+
     }
 }
